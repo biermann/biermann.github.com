058cc3de4b841899eac8ae700640d0a6998844e0
biermann <biermann@users.noreply.github.com>
2013-11-27 10:29:03 +0100

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -312,7 +312,7 @@ nmp.storage.init= function(e) {
         for (var i in nmp.storage.field) {
             console.log(nmp.storage.field[i]+': '+obj[nmp.storage.field[i]]);
 	   if (typeof obj[nmp.storage.field[i]] == 'undefined'){obj[nmp.storage.field[i]]  = '';console.log('problem: '+obj[nmp.storage.field[i]]);}
-           if (typeof obj[nmp.storage.field[view]] == ''){obj[nmp.storage.field[view]]  = 'status';console.log('problem: '+obj[nmp.storage.field[i]]);}
+           if (typeof obj[nmp.storage.field["view"]] == ''){obj[nmp.storage.field["view"]]  = 'status';console.log('problem: '+obj[nmp.storage.field[i]]);}
         }
            
             nmp.storage.currentSet(obj);
