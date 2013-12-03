5da9cc422be393f5b78cef15a93bb4c6902fe89b
biermann <pbantispamaccount@mac.com>
2013-12-03 23:42:57 +0100

    commit

--- js/touch.js
+++ js/touch.js
@@ -1,13 +1,12 @@
 nmp.touch.init = function(e) {
 
 
-window.addEventListener('load', function(){ // on page load
  
  document.body.addEventListener('touchstart', function(e){
   alert(e.changedTouches[0].pageX) // alert pageX coordinate of touch point
+   console.log( 'nmp.touch');
  }, false)
  
-}, false)
 
 
 
