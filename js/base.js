68159a9569612649c78a4fb53efdd5b409617ee7
biermann <biederp@mac.com>
2017-01-09 22:16:09 +0100

    fix

--- js/base.js
+++ js/base.js
@@ -54,7 +54,6 @@ Object.defineProperty(nmp.app.radio, 'readonlyObj', {
   writable: false, 
   configurable: false,
   value:   [
-  { objId: "9", src: "http://listen.radionomy.com/jmaster", desc: "test fav", www: 'http://www.rjm-radio.fr/radio-groove-jazz-funk/', type: "audio/mpeg", objOwner: "browser", fav: "true" },
   { objId: "8", src: "http://ice.somafm.com/sf1033", desc: "SF 10-33", www: 'http://somafm.com/', type: "audio/mpeg", objOwner: "biermann" },
   { objId: "7", src: "http://internationalradiofestival.ice.infomaniak.ch/radio-live.mp3", desc: "IRF", www: 'http://www.internationalradiofest.com/', type: "audio/mpeg", objOwner: "biermann" },
   { objId: "6", src: "http://germany-rinse.it.two.dnbradio.com:10128", desc: "DNBRadio", www: 'http://dnbradio.com/', type: "audio/mpeg", objOwner: "biermann" },
