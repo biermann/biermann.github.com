c042c5d2140bc6e14190f789d0c680461e1fa967
biermann <biederp@mac.com>
2017-10-04 20:01:50 +0200

    commi

--- js/base.js
+++ js/base.js
@@ -4,7 +4,7 @@ nmp.app = {};
 Object.defineProperty(nmp.app, 'version', {
   enumerable: true, 
   writable: false, 
-  value: "20170414", 
+  value: "20171004", 
   configurable: false
 });
 
@@ -55,6 +55,8 @@ Object.defineProperty(nmp.app.radio, 'readonlyObj', {
   configurable: false,
   value:   [
     
+  { objId: "11", src: "https://audioasyl.net/m3u/live.php", desc: "Audioasyl.m3u for iOS11", www: 'http://audioasyl.net/', type: "audio/x-mpequrl", objOwner: "biermann" },
+  { objId: "10", src: "http://stream.srg-ssr.ch/drsvirus/mp3_128.m3u", desc: "Virus.m3u for iOS11", www: 'http://www.virus.ch/', type: "audio/x-mpequrl", objOwner: "biermann" },
   { objId: "9", src: "http://stream.stadtfilter.net:8406/stadtfilter.mp3", desc: "Stadtfilter", www: 'http://stadtfilter.net/', type: "audio/mpeg", objOwner: "biermann" },
   { objId: "8", src: "http://ice.somafm.com/sf1033", desc: "SF 10-33", www: 'http://somafm.com/', type: "audio/mpeg", objOwner: "biermann" },
   { objId: "7", src: "http://internationalradiofestival.ice.infomaniak.ch/radio-live.mp3", desc: "IRF", www: 'http://www.internationalradiofest.com/', type: "audio/mpeg", objOwner: "biermann" },
